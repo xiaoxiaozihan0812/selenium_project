653c4c375309dc6afecfe19f6cc7fe295084153e
xiaoxiaozihan0812 <67266753+xiaoxiaozihan0812@users.noreply.github.com>
2020-06-22 18:19:16 +0800

    Create auto_study.py

---
+++ auto_study.py
@@ -0,0 +1,241 @@
+#!/user/bin/env python
+# -*- coding: utf-8 -*-
+# @Time     : 2019/9/15 18:30
+# @Author   : zihan.zhao
+# @File     : auto_study.pu
+# @Software : PyCharm
+import os
+os.environ['NLS_LANG'] = 'SIMPLIFIED CHINESE_CHINA.UTF8'
+import re
+import time
+import sys
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+import pymysql
+import tkinter as tk
+from PIL import  Image,ImageEnhance
+import threading
+import pytesseract
+import chaojiying
+
+
+
+def indexhtml(user,password):
+
+    driver.find_element_by_xpath('//input[@type="text"]').send_keys(user)
+    driver.find_element_by_xpath('//input[@type="password"]').send_keys(password)
+
+
+def getcaptcha():
+    element = driver.find_element_by_id("verifyCanvas")
+    driver.save_screenshot(r'./screenshot.png')
+    left = element.location['x']
+    top = element.location['y']
+    right = element.location['x'] + element.size['width']
+    bottom = element.location['y'] + element.size['height']
+    im = Image.open(r'./screenshot.png')
+    im = im.crop((left, top, right, bottom))
+    im.save(r'./screenshot.png')
+
+def use_getcaptcha():
+    im = open(r'./screenshot.png', 'rb').read()
+    capt=chaojiying.PostPic(im, 4004)
+    os.remove(r'./screenshot.png')
+    captcha=capt['pic_str']
+    return captcha
+
+def login(captcha,user):
+    driver.find_element_by_name('password').send_keys(str(captcha))
+    driver.find_element_by_class_name('logC_btn').click()
+    print('请等待此账号提示登录成功，提示成功后即可打开新的程序来登录新账号')
+    lb.insert(tk.END, '请等待此账号提示登录成功，提示成功后即可打开新的程序来登录新账号')
+    time.sleep(4)
+    if len(driver.find_elements_by_xpath('//div[@class="title"]')) <3:
+        print('WARNING:验证码识别错误，请关闭程序重新启动程序进行账号登录!!!')
+        lb.insert(tk.END, 'WARNING:验证码识别错误，请关闭程序重新启动程序进行账号登录!!!')
+
+    else:
+        print('请提醒账号主人,自动学习期间请不要登录此账号，否则程序会被中断')
+        lb.insert(tk.END, '请提醒账号主人,自动学习期间请不要登录此账号，否则程序会被中断')
+        print('账号', user, '登录成功，开始学习')
+        lb.insert(tk.END, '账号' + user + '登录成功，开始学习')
+        startlearn(user)
+def startlearn(user):
+    time.sleep(2)
+    if len(driver.find_elements_by_xpath('//div[@class="title"]'))>1:
+        try:
+            print('一共查询到有', str(len(driver.find_elements_by_xpath('//div[@class="title"]'))), '门课程')
+            lb.insert(tk.END, '一共查询到有'+ str(len(driver.find_elements_by_xpath('//div[@class="title"]')))+'门课程',)
+            for i in range(len(driver.find_elements_by_xpath('//div[@class="title"]'))):
+                if '100' in driver.find_elements_by_xpath('//span[@class="jdb"]')[i].text:
+                    print('第',str(i+1),'门课程',driver.find_elements_by_xpath('//div[@class="title"]')[i].text,'已经学完')
+                    lb.insert(tk.END, '第'+str(i+1)+'门课程'+driver.find_elements_by_xpath('//div[@class="title"]')[i].text+'已经学完')
+                else:
+                    print('进入第' ,str(i + 1) ,'门课程' , driver.find_elements_by_xpath('//div[@class="title"]')[i].text)
+                    lb.insert(tk.END, '进入第' +str(i + 1) +'门课程' + driver.find_elements_by_xpath('//div[@class="title"]')[i].text)
+                    driver.refresh()
+                    time.sleep(2)
+                    subject=driver.find_elements_by_xpath('//div[@class="title"]')[i].text
+
+                    driver.find_elements_by_xpath('//div[@class="title"]')[i].click()
+
+                    for j in driver.find_elements_by_xpath('//li[@class="chapterItem"]'):
+                        j.click()
+                        time.sleep(2)
+
+                    for k in range(len(driver.find_elements_by_xpath('//li[@class="setionItem"]'))):
+                        print('开始学习第',str(i+1),'门课程',subject,'的第',str(k+1),'节课...')
+                        lb.insert(tk.END, '开始学习第'+str(i+1)+'门课程'+subject+'的第'+str(k+1)+'节课...',)
+                        if '100' in driver.find_elements_by_xpath('//span[@class="jdb"]')[k].text:
+                            print('第',str(i+1),'门课程',subject,'的第',str(k+1),'节课','已经学完，开始学习下一小节')
+                            lb.insert(tk.END, '第'+str(i+1)+'门课程'+subject+'的第'+str(k+1)+'节课'+'已经学完，开始学习下一小节')
+                        else:
+                            element=driver.find_elements_by_xpath('//li[@class="setionItem"]')[k]
+                            driver.execute_script("arguments[0].click();", element)
+                            answerquestion()
+                            wait.until(EC.visibility_of_element_located((By.XPATH,'//div[@class="backbtn btn"]')))
+                            time.sleep(5)
+                            driver.find_element_by_xpath('//div[@class="backbtn btn"]').click()
+                            time.sleep(5)
+                            for j in driver.find_elements_by_xpath('//li[@class="chapterItem"]'):
+                                j.click()
+                                time.sleep(2)
+                    driver.find_element_by_xpath('//li[@class="childActive"]').click()
+
+            print(user,'学习结束')
+            lb.insert(tk.END, user+'学习结束',)
+            time.sleep(2)
+            driver.quit()
+
+        except:
+            print('WARNING:网络波动，页面长时间未能加载，正在尝试重新运行!!!')
+            lb.insert(tk.END, 'WARNING:网络波动，页面长时间未能加载，正在尝试重新运行!!!')
+            driver.get('http://zzx.ouchn.edu.cn/edu/public/student/#/courseList/1')
+            time.sleep(10)
+            startlearn(user)
+    else:
+        print('WARNING:网络波动，页面长时间未能加载，正在尝试重新运行!!!')
+        lb.insert(tk.END, 'WARNING:网络波动，页面长时间未能加载，正在尝试重新运行!!!')
+        driver.get('http://zzx.ouchn.edu.cn/edu/public/student/#/courseList/1')
+        time.sleep(10)
+        startlearn(user)
+
+def answerquestion():
+    time.sleep(5)
+    if len(driver.find_elements_by_xpath('//div[@class="see"]')) ==0:
+        print('此课程无题')
+        lb.insert(tk.END,'此课程无题')
+        print('等待视频播放结束自动播放下节视频')
+        lb.insert(tk.END, '等待视频播放结束自动播放下节视频')
+    else:
+        print('此课程有题，开始答题...')
+        lb.insert(tk.END, '此课程有题，开始答题...')
+        # print('查看正确答案')
+        # lb.insert(tk.END, '查看正确答案')
+        elements=driver.find_elements_by_xpath('//div[@class="see"]')
+        for i in elements:
+            i.click()
+
+        print('正在自动选择正确答案...')
+        lb.insert(tk.END, '正在自动选择正确答案...')
+        time.sleep(2)
+        for j in range(len(driver.find_elements_by_xpath('//div[@class="subject"]'))):
+            answer=driver.find_elements_by_xpath('//div[@class="right_key"]//span')[j].text
+            print(answer)
+            lb.insert(tk.END, answer)
+            if 'A' in answer:
+                driver.find_elements_by_xpath('//div[@class="subject"]')[j].find_elements_by_xpath('.//div[@class="answer"]')[0].click()
+
+            if 'B' in answer:
+                driver.find_elements_by_xpath('//div[@class="subject"]')[j].find_elements_by_xpath('.//div[@class="answer"]')[1].click()
+
+            if 'C' in answer:
+                driver.find_elements_by_xpath('//div[@class="subject"]')[j].find_elements_by_xpath('.//div[@class="answer"]')[2].click()
+
+            if 'D' in answer:
+                driver.find_elements_by_xpath('//div[@class="subject"]')[j].find_elements_by_xpath('.//div[@class="answer"]')[3].click()
+
+            if 'E' in answer:
+                driver.find_elements_by_xpath('//div[@class="subject"]')[j].find_elements_by_xpath('.//div[@class="answer"]')[4].click()
+
+            if 'F' in answer:
+                driver.find_elements_by_xpath('//div[@class="subject"]')[j].find_elements_by_xpath('.//div[@class="answer"]')[5].click()
+
+            if 'G' in answer:
+                driver.find_elements_by_xpath('//div[@class="subject"]')[j].find_elements_by_xpath('.//div[@class="answer"]')[6].click()
+
+            if 'H' in answer:
+                driver.find_elements_by_xpath('//div[@class="subject"]')[j].find_elements_by_xpath('.//div[@class="answer"]')[7].click()
+
+            if 'I' in answer:
+                driver.find_elements_by_xpath('//div[@class="subject"]')[j].find_elements_by_xpath('.//div[@class="answer"]')[8].click()
+            time.sleep(1)
+        print('答题结束，等待学习视频播放完毕，自动学习下面的视频')
+        lb.insert(tk.END, '答题结束，等待学习视频播放完毕，自动学习下面的视频')
+
+
+
+
+def thread_it(func):
+    '''将函数放入线程中执行'''
+    # 创建线程
+    t = threading.Thread(target=func)
+    # 守护线程
+    t.setDaemon(True)
+    # 启动线程
+    t.start()
+
+
+def run():
+    user = inputuser.get()
+    password = inputpassword.get()
+    print('请保持网络状态良好，同时登录多个账号观看视频，会占用电脑很大的带宽，不建议使用wifi，建议电脑连接网线，网络状态差会影响学习进度,')
+    lb.insert(tk.END, '请保持网络状态良好，同时登录多个账号观看视频，会占用电脑很大的带宽，不建议使用wifi，建议电脑连接网线，网络状态差会影响学习进度,')
+    indexhtml(user,password)
+    getcaptcha()
+    login(use_getcaptcha(),user)
+
+
+if __name__ == '__main__':
+    # 第1步，实例化object，建立窗口window
+    window = tk.Tk()
+    # 第2步，给窗口的可视化起名字
+    window.title('Auto Study program - V1.0')
+    window.geometry("700x800")
+    labeluser = tk.Label(window, text="请输入账号")
+    labeluser.pack()
+    inputuser = tk.Entry(window, width=30)
+    inputuser.pack()
+    labelpassword = tk.Label(window, text="请输入密码")
+    labelpassword.pack()
+    inputpassword = tk.Entry(window, width=30)
+    inputpassword.pack()
+    btn = tk.Button(window, text='Login', command=lambda: thread_it(run))
+    btn.pack()
+    lb = tk.Listbox(window)
+    scr = tk.Scrollbar(window)
+    lb.config(yscrollcommand=scr.set)
+    scr.config(command=lb.yview)
+    lb.pack(side=tk.LEFT, expand=tk.YES,fill=tk.BOTH)
+    scr.pack(side=tk.RIGHT, fill=tk.Y)
+    chaojiying = chaojiying.Chaojiying_Client('')
+    chrome_options = Options()
+    # # 设置chrome浏览器无界面模式
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument("--mute-audio")
+    # webdriver.Firefox(executable_path='.\Google\Chrome\Application\chrome.exe')
+    driver = webdriver.Chrome(r'./settings/chromedriver.exe', chrome_options=chrome_options)
+    driver.maximize_window()
+    driver.implicitly_wait(120)
+    wait = WebDriverWait(driver, 3600, 0.5)
+    driver.get('http://zzx.ouchn.edu.cn/edu/public/student/')
+    window.mainloop()
+
+
+
+
+
